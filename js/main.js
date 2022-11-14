d33609a41d5f9bc7cce3a12360908ac5e0d7ae75
jeremymillet <jeremy.millet37@gmail.com>
2022-11-14 17:32:07 +0100

    techno add + responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 /*recuperation des img background*/
-
-
-const imgHome = document.querySelector('.background-home')
-const imgDestination = document.querySelector('.background-destination')
-const imgCrew = document.querySelector('.background-crew')
-const imgTechnology = document.querySelector('.background-technology')
 const moon = document.getElementById('moon')
 const mars = document.getElementById('mars')
 const europa = document.getElementById('europa')
@@ -13,147 +7,118 @@ const home = document.getElementById('home')
 const commander = document.getElementById('commander')
 const missionSpecialist = document.getElementById('mission-specialist')
 const pilot = document.getElementById('pilot')
-const flightEngineer= document.getElementById('flight-engineer')
+const flightEngineer = document.getElementById('flight-engineer')
+const spaceport = document.getElementById('spaceport')
+const launchVehicle = document.getElementById('launch-vehicle')
+const spaceCapsule = document.getElementById('space-capsule')
 
 
 
-
-
-function displayImgHome(){
-    if (imgHome.style.display='none'){
-        imgHome.style.display='flex'}
+function homeOn() {
+    { home.style.display = "flex" }
 }
-function displayImgNoneHome(){
-    if (imgHome.style.display='flex'){
-        imgHome.style.display='none'}
-}
-function homeOn(){
-    if (home.style.display='none'){
-        home.style.display='flex'}
-}
-function homeNone(){
-    if (home.style.display='flex'){
-        home.style.display='none'}
+function homeNone() {
+    { home.style.display = "none" }
 }
 
-function displayImgDestination(){
-    if (imgDestination.style.display='none'){
-        imgDestination.style.display='flex'}
-}
-function displayImgNoneDestination(){
-    if (imgDestination.style.display='flex'){
-        imgDestination.style.display='none'}
-}
 function moonOn() {
-    if (moon.style.display='none'){
-        moon.style.display='flex'
+    {
+        moon.style.display = 'flex'
     }
 }
 function moonNone() {
-    if (moon.style.display='flex'){
-        moon.style.display='none'
+    {
+        moon.style.display = 'none'
     }
 }
 function marsOn() {
-    if (mars.style.display='none'){
-        mars.style.display='flex'
+    {
+        mars.style.display = 'flex'
     }
 }
 function marsNone() {
-    if (mars.style.display='flex'){
-        mars.style.display='none'
+    {
+        mars.style.display = 'none'
     }
 }
 function europaOn() {
-    if (europa.style.display='none'){
-        europa.style.display='flex'
+    {
+        europa.style.display = 'flex'
     }
 }
 function europaNone() {
-    if (europa.style.display='flex'){
-        europa.style.display='none'
+    {
+        europa.style.display = 'none'
     }
 }
 function titanOn() {
-    if (titan.style.display='none'){
-        titan.style.display='flex'
+    {
+        titan.style.display = 'flex'
     }
 }
 function titanNone() {
-    if (titan.style.display='flex'){
-        titan.style.display='none'
+    {
+        titan.style.display = 'none'
     }
 }
-function displayImgCrew(){
-    if (imgCrew.style.display='none'){
-        imgCrew.style.display='flex'}
-}
-function displayImgNoneCrew(){
-    if (imgCrew.style.display='flex'){
-        imgCrew.style.display='none'}
-}
+
 function commanderOn() {
-    if (commander.style.display='none'){
-        commander.style.display='flex'
+    {
+        commander.style.display = 'flex'
     }
 }
 function commanderNone() {
-    if (commander.style.display='flex'){
-        commander.style.display='none'
+    {
+        commander.style.display = 'none'
     }
 }
 function missionSpecialistOn() {
-    if (missionSpecialist.style.display='none'){
-        missionSpecialist.style.display='flex'
+    {
+        missionSpecialist.style.display = 'flex'
     }
 }
 function missionSpecialistNone() {
-    if (missionSpecialist.style.display='flex'){
-        missionSpecialist.style.display='none'
+    {
+        missionSpecialist.style.display = 'none'
     }
 }
 function pilotOn() {
-    if (pilot.style.display='none'){
-        pilot.style.display='flex'
+    {
+        pilot.style.display = 'flex'
     }
 }
 function pilotNone() {
-    if (pilot.style.display='flex'){
-        pilot.style.display='none'
+    {
+        pilot.style.display = 'none'
     }
 }
 function flightEngineerOn() {
-    if (flightEngineer.style.display='none'){
-        flightEngineer.style.display='flex'
+    {
+        flightEngineer.style.display = 'flex'
     }
 }
 function flightEngineerNone() {
-    if (flightEngineer.style.display='flex'){
-        flightEngineer.style.display='none'
+    {
+        flightEngineer.style.display = 'none'
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function displayImgTechnology(){
-    if (imgTechnology.style.display='none'){
-        imgTechnology.style.display='flex'}
+function spaceportOn(){
+    spaceport.style.display = 'flex'
+}
+function spaceportNone(){
+    spaceport.style.display = 'none'
 }
 
-function displayImgNoneTechnology(){
-    if (imgTechnology.style.display='flex'){
-        imgTechnology.style.display='none'}
+function spaceCapsuleOn(){
+    spaceCapsule.style.display = 'flex'
 }
-
+function spaceCapsuleNone(){
+    spaceCapsule.style.display = 'none'
+}
+function launchVehicleOn(){
+    launchVehicle.style.display = 'flex'
+}
+function launchVehicleNone(){
+    launchVehicle.style.display = 'none'
+}
