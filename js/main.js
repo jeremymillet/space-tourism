63fe4498b4aa45d33c8becf7794feac4a9a04cff
jeremymillet <jeremy.millet37@gmail.com>
2022-11-17 17:30:32 +0100

    version tablette

--- js/main.js
+++ js/main.js
@@ -11,114 +11,22 @@ const flightEngineer = document.getElementById('flight-engineer')
 const spaceport = document.getElementById('spaceport')
 const launchVehicle = document.getElementById('launch-vehicle')
 const spaceCapsule = document.getElementById('space-capsule')
+const allPages = [moon,mars,europa,titan,home,commander,missionSpecialist,pilot,flightEngineer,spaceport,launchVehicle,spaceCapsule]
 
+let currentPage = ''
 
+function handlePage(pageClicked) {
 
-function homeOn() {
-    { home.style.display = "flex" }
-}
-function homeNone() {
-    { home.style.display = "none" }
-}
+    const elementClicked = document.getElementById(pageClicked)
 
-function moonOn() {
-    {
-        moon.style.display = 'flex'
+    if (elementClicked.style.display === 'flex' && currentPage !== elementClicked.id) {
+        elementClicked.style.display = 'none'
+    } else {
+        elementClicked.style.display = 'flex'
+        currentPage = elementClicked.id
+        // désactive tous les autres
+        allPages.forEach(page => {
+            if (page.id !== elementClicked.id) page.style.display = 'none'
+        })
     }
-}
-function moonNone() {
-    {
-        moon.style.display = 'none'
-    }
-}
-function marsOn() {
-    {
-        mars.style.display = 'flex'
-    }
-}
-function marsNone() {
-    {
-        mars.style.display = 'none'
-    }
-}
-function europaOn() {
-    {
-        europa.style.display = 'flex'
-    }
-}
-function europaNone() {
-    {
-        europa.style.display = 'none'
-    }
-}
-function titanOn() {
-    {
-        titan.style.display = 'flex'
-    }
-}
-function titanNone() {
-    {
-        titan.style.display = 'none'
-    }
-}
-
-function commanderOn() {
-    {
-        commander.style.display = 'flex'
-    }
-}
-function commanderNone() {
-    {
-        commander.style.display = 'none'
-    }
-}
-function missionSpecialistOn() {
-    {
-        missionSpecialist.style.display = 'flex'
-    }
-}
-function missionSpecialistNone() {
-    {
-        missionSpecialist.style.display = 'none'
-    }
-}
-function pilotOn() {
-    {
-        pilot.style.display = 'flex'
-    }
-}
-function pilotNone() {
-    {
-        pilot.style.display = 'none'
-    }
-}
-function flightEngineerOn() {
-    {
-        flightEngineer.style.display = 'flex'
-    }
-}
-function flightEngineerNone() {
-    {
-        flightEngineer.style.display = 'none'
-    }
-}
-
-function spaceportOn(){
-    spaceport.style.display = 'flex'
-}
-function spaceportNone(){
-    spaceport.style.display = 'none'
-}
-
-function spaceCapsuleOn(){
-    spaceCapsule.style.display = 'flex'
-}
-function spaceCapsuleNone(){
-    spaceCapsule.style.display = 'none'
-}
-function launchVehicleOn(){
-    launchVehicle.style.display = 'flex'
-}
-function launchVehicleNone(){
-    launchVehicle.style.display = 'none'
 }
